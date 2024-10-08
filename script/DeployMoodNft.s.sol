9bc5ada0c75c1f3167622ed2be36d6a6fbd352e5
boobaGreen <claudiodallara77@gmail.com>
2024-10-08 21:21:10 +0200

    16 fino a minuto 13e38

---
+++ script/DeployMoodNft.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script, console} from "forge-std/Script.sol";
+import {MoodNft} from "../src/MoodNft.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract DeployMoodNft is Script {
+    function run() external returns (MoodNft) {
+        string memory sadSvg = vm.readFile("./img/sad.svg");
+        string memory happySvg = vm.readFile("./img/happy.svg");
+        vm.startBroadcast();
+        MoodNft moodNft = new MoodNft(
+            svgToImageURI(sadSvg),
+            svgToImageURI(happySvg)
+        );
+        vm.stopBroadcast();
+        return moodNft;
+    }
+
+    function svgToImageURI(
+        string memory svg
+    ) public pure returns (string memory) {
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg)))
+        );
+        return string(abi.encodePacked(baseURL, svgBase64Encoded));
+    }
+}
---
+++ test/DeployMoodNftTest.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DeployMoodNft} from "../script/DeployMoodNft.s.sol";
+
+contract DeployMoodNftTest is Test {
+    DeployMoodNft public deployer;
+
+    function setUp() public {
+        deployer = new DeployMoodNft();
+    }
+
+    function testCovertSvgToUri() public view {
+        string
+            memory expectedUri = "data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjQwIiB3aWR0aD0iMjAwIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjx0ZXh0IHg9IjUiIHk9IjMwIiBmaWxsPSJub25lIiBzdHJva2U9InJlZCIgZm9udC1zaXplPSIzNSI+SSBsb3ZlIFNWRyE8L3RleHQ+PC9zdmc+";
+        string
+            memory svg = '<svg height="40" width="200" xmlns="http://www.w3.org/2000/svg"><text x="5" y="30" fill="none" stroke="red" font-size="35">I love SVG!</text></svg>';
+        string memory actualUri = deployer.svgToImageURI(svg);
+        console.log("expectedUri: ", expectedUri);
+        console.log("actualUri: ", actualUri);
+        assert(
+            keccak256(abi.encodePacked(expectedUri)) ==
+                keccak256(abi.encodePacked(actualUri))
+        );
+    }
+}
