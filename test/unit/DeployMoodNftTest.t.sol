9b9758c7a4c80f5e4eed7f9050eaf658ba9ff8d8
boobaGreen <claudiodallara77@gmail.com>
2024-10-08 23:02:48 +0200

    modules

--- test/unit/DeployMoodNftTest.t.sol
+++ test/unit/DeployMoodNftTest.t.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
-import {DeployMoodNft} from "../script/DeployMoodNft.s.sol";
+import {DeployMoodNft} from "../../script/DeployMoodNft.s.sol";
 
 contract DeployMoodNftTest is Test {
     DeployMoodNft public deployer;
